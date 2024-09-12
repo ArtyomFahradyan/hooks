8e44aff83dc71683f54d2e115c2a84d64ce68d4e
ArtyomFahradyan <artyom.fahradyan9554@gmail.com>
2024-09-12 09:45:59 +0400

    Fix enumToArray

--- src/helpers/arrayHelpers.ts
+++ src/helpers/arrayHelpers.ts
@@ -1,14 +1,15 @@
 /**
  * Converts enum to array
  */
-type Enum<E> = Record<keyof E, string | number> & {
+export type Enum<E> = Record<keyof E, string | number> & {
   [k: string]: string | number;
 };
+
 export function enumToArray<E extends Enum<E>>(enumObject: E) {
   const enumArray = [];
 
   for (const key in enumObject) {
-    enumArray.push({ type: enumObject[key] });
+    enumArray.push(enumObject[key]);
   }
 
   return enumArray;
